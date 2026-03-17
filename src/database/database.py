c355d0e71b386ef64d049a46905a7cdcc9688151
florian-d-agostino <florian.d-agostino@laplateforme.io>
2026-03-17 16:10:42 +0100

    feat(database): Fix History Table - add a type and category - FD

--- src/database/database.py
+++ src/database/database.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 class Database:
     def __init__(self, db_name="budget_buddy.db"):
-        # On s'assure que la base de données est créée dans le même dossier que ce fichier
         db_path = Path(__file__).parent / db_name
         self.conn = sqlite3.connect(db_path)
         self.cursor = self.conn.cursor()
@@ -13,25 +12,26 @@ class Database:
         # Activation des contraintes de clés étrangères (obligatoire sur SQLite)
         self.cursor.execute("PRAGMA foreign_keys = ON;")
 
-        # Table CUSTOMER (Client)
+        # Table CUSTOMER 
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS customer (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL,
-                email TEXT UNIQUE NOT NULL,
+                login TEXT NOT NULL,
                 password TEXT NOT NULL,
+                name TEXT NOT NULL,
+                first_name TEXT NOT NULL,
+                email TEXT UNIQUE NOT NULL,
                 created_at DATETIME DEFAULT CURRENT_TIMESTAMP
             )
         ''')
 
-        # Table ACCOUNT (Compte bancaire)
+        # Table ACCOUNT 
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS account (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 customer_id INTEGER NOT NULL,
                 account_type TEXT NOT NULL,
-                balance REAL DEFAULT 0.0,
+                pay REAL DEFAULT 10.0,
                 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE
             )
@@ -42,8 +42,9 @@ class Database:
             CREATE TABLE IF NOT EXISTS history (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 account_id INTEGER NOT NULL,
-                transaction_type TEXT NOT NULL, 
+                transaction_type TEXT CHECK(transaction_type IN ('depot', 'retrait', 'virement')) NOT NULL, 
                 amount REAL NOT NULL,
+                category TEXT NOT NULL,
                 description TEXT,
                 date DATETIME DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE
@@ -56,7 +57,7 @@ class Database:
         self.conn.close()
 
 if __name__ == "__main__":
-    # Test simple pour vérifier que la base se crée bien
+    # Test
     db = Database()
     print("Base de données SQLite et tables créées avec succès !")
     db.close()
